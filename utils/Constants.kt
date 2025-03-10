5c3ca46d7c2fe4832cdac6dfc767787ec3b9e9d9
binny <90454079+binny3213@users.noreply.github.com>
2025-03-10 08:20:30 +0200

    Update Constants.kt

--- utils/Constants.kt
+++ utils/Constants.kt
@@ -3,9 +3,7 @@ package com.example.planitgo_finalproject.utils
 class Constants {
     companion object{
         const val GEOAPIFY_URL = "https://api.geoapify.com/"
-        const val GEOAPIFY_API_KEY = "8566a5e719f74c77becf869de311696b"
         const val COUNTRY_URL = "https://restcountries.com/v3.1/"
         const val PLACES_URL = "https://maps.googleapis.com/maps/api/"
-        const val PLACES_API_KEY = "AIzaSyANA9QMCXvtjhXSchw4g3aN3ekZioJzGpA"
     }
 }
